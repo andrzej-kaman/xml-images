eccc1450e437edd0ac6346fa9036afd9eb196557
andrzej-kaman <andrzej@kamanmarketing.pl>
2026-05-20 23:19:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -31,8 +31,10 @@ client = genai.Client(
 )
 
 # Modele AI
-TEXT_ANALYSIS_MODEL = "gemini-2.5-flash" # TEXT_ANALYSIS_MODEL = "gemini-2.5-pro" -> do dokładniejszej analizy 
-IMAGE_GENERATION_MODEL = "gemini-3.1-flash-image-preview" # IMAGE_GENERATION_MODEL = "gemini-3-pro-image-preview" -> do zdjęć wysokiej jakości
+TEXT_ANALYSIS_MODEL = "gemini-2.5-flash" 
+# TEXT_ANALYSIS_MODEL = "gemini-2.5-pro" -> do dokładniejszej analizy 
+IMAGE_GENERATION_MODEL = "gemini-3.1-flash-image-preview" 
+# IMAGE_GENERATION_MODEL = "gemini-3-pro-image-preview" -> do zdjęć wysokiej jakości
 
 # Foldery tymczasowe
 TEMP_FOLDER = 'temp_files'
